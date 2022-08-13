4d5c34ae8357e06285d7831f533120a74f8e2188
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-08-13 19:33:25 +0200

    husky setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,10 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return ( 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
 export default MyApp;
