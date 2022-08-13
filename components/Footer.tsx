7cea920ab2cafc65f10829d22a7468dd3a2625a1
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-08-13 19:35:07 +0200

    refactor footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Footer = () => {
-  return (
-    <section id="footer" className="bg-black text-white pt-20 pb-12">
-      <p className="text-center text-lg">@2022 All rights reserved</p>
-    </section>
-  );
-};
+const Footer = () => (
+  <section id="footer" className="bg-black text-white pt-20 pb-12">
+    <p className="text-center text-lg">@2022 All rights reserved</p>
+  </section>
+);
 
 export default Footer;
