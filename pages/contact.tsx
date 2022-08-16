fa36e0de503237fa41993d6025b57c0d6c22785d
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-08-16 17:55:33 +0200

    add responsiveness

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ContactForm from '../components/ContactForm';
 
 const contact = () => (
-  <div className="container max-w-[800px] mx-auto font-light mb-40">
+  <div className="container max-w-[800px] mx-auto font-light mb-40 px-5">
     <h1 className="text-4xl text-center mt-16">Contact</h1>
-    <p className="mt-10">Email: info@aguasdeibiza.com</p>
-    <p className="mt-2">Phone: +34 971 31 99 91</p>
+    <div className="flex items-end">
+      <p className="mt-10">Email:</p>
+      <a href="mailto:info@aguasdeibiza.com" className="ml-3 underline">
+        info@aguasdeibiza.com
+      </a>
+    </div>
+    <div className="flex items-end">
+      <p className="mt-2">Phone:</p>
+      <a href="tel:971-319-991" className="ml-3 underline">
+        +34 971 31 99 91
+      </a>
+    </div>
     <ContactForm />
   </div>
 );
