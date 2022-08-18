4a8a296f2381773cad44bdd05d80a245252c26d2
Jakub Zakowicz <63516552+JakubZakowicz@users.noreply.github.com>
2022-08-18 20:42:35 +0200

    Add room booking page (#7)
    * add booking page
    
    * add seo to contact page
    
    * add responsiveness

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,20 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 import ContactForm from '../components/ContactForm';
+import Seo from '../components/Seo';
 
 const contact = () => (
   <div className="container max-w-[800px] mx-auto font-light mb-40 px-5">
+    <Seo title="Contact" />
     <h1 className="text-4xl text-center mt-16">Contact</h1>
     <div className="flex items-end">
       <p className="mt-10">Email:</p>
-      <a href="mailto:info@aguasdeibiza.com" className="ml-3 underline">
-        info@aguasdeibiza.com
-      </a>
+      <Link href="mailto:info@aguasdeibiza.com">
+        <a className="ml-3 underline">info@aguasdeibiza.com</a>
+      </Link>
     </div>
     <div className="flex items-end">
       <p className="mt-2">Phone:</p>
-      <a href="tel:971-319-991" className="ml-3 underline">
-        +34 971 31 99 91
-      </a>
+      <Link href="tel:971-319-991">
+        <a className="ml-3 underline">+34 971 31 99 91</a>
+      </Link>
     </div>
     <ContactForm />
   </div>
---
+++ pages/room-booking.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { MdArrowDropDown } from '@react-icons/all-files/md/MdArrowDropDown';
+import { RiCupFill } from '@react-icons/all-files/ri/RiCupFill';
+import { AiFillCreditCard } from '@react-icons/all-files/ai/AiFillCreditCard';
+import Seo from '../components/Seo';
+import Booking from '../components/Booking';
+import RoomImg from '../public/images/room-details.png';
+
+const amenities = [
+  {
+    id: 1,
+    name: 'Separate Shower',
+  },
+  {
+    id: 2,
+    name: 'Air Conditioning',
+  },
+  {
+    id: 3,
+    name: 'Desk of Workplace',
+  },
+  {
+    id: 4,
+    name: 'Free Wi-Fi',
+  },
+  {
+    id: 5,
+    name: 'Bathub',
+  },
+];
+
+const RoomBooking = () => (
+  <div className="container max-w-container mx-auto px-5 2xl:px-0">
+    <Seo title="Room Booking" />
+    <div className="mt-10">
+      <Booking />
+    </div>
+    <div className="flex gap-20 xl:w-[1090px] mt-10 border border-black px-20 py-5">
+      <div>
+        <div className="flex">
+          <p>View Results By</p>
+          <MdArrowDropDown className="-mt-1" size="30" />
+        </div>
+        <p className="font-medium">Rooms</p>
+      </div>
+      <div>
+        <div className="flex">
+          <p>Sort By</p>
+          <MdArrowDropDown className="-mt-1" size="30" />
+        </div>
+        <p className="font-medium">Recommended</p>
+      </div>
+    </div>
+    <div className="border border-black mt-12 xl:w-[1090px] p-5 mb-20">
+      <div className="flex flex-col xl:flex-row gap-5">
+        <div className="w-full xl:w-[500px] xl:h-[166px] overflow-hidden">
+          <Image
+            src={RoomImg}
+            width="500px"
+            height="250px"
+            layout="responsive"
+          />
+        </div>
+        <div className="border-b-2 w-full pb-7">
+          <p className="font-medium text-2xl">Premier Room</p>
+          <p className="font-light mt-2">
+            Romantic style room, offers a private terrace with mountain view.
+            Spa access included
+          </p>
+          <div className="underline mt-5">Room Details</div>
+        </div>
+      </div>
+      <div className="md:flex mt-8">
+        <div className="md:w-[330px] xl:w-[500px] border-b-2 pb-7 md:border-b-0 md:border-r-2 px-5">
+          <p className="text-3xl">Amenities</p>
+          <ul className="font-light list-disc ml-5">
+            {amenities.map((amenity) => (
+              <li key={amenity.id} className="mt-3">
+                {amenity.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="pl-6 w-full font-light mt-10 md:mt-0">
+          <div className="flex flex-wrap justify-between gap-4">
+            <div>
+              <p className="font-medium text-2xl">Flexible Rage</p>
+              <div className="flex gap-5 items-center font-medium mt-2">
+                <RiCupFill size="20" />
+                <p>Breakfast included</p>
+              </div>
+              <div className="flex gap-5 items-center font-medium mt-2">
+                <AiFillCreditCard size="20" />
+                <p>Guaranted with Credit Card</p>
+              </div>
+            </div>
+            <div className="sm:text-right">
+              <p className="text-2xl font-medium">$840</p>
+              <p className="">Per Night</p>
+              <p className="text-gray-500">Including Taxes & Fees</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap justify-between gap-5 items-start mt-5">
+            <p className="w-3/5">
+              Rate includes breakfast, spa access and WiFi. Touristic Tax not
+              included. Free cancellation up to 3 days before the day of
+              arrival, subject to full penalty.
+            </p>
+            <Link href="#">
+              <a className="bg-dark-gray text-white text-2xl py-3 px-6">
+                Book Now
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default RoomBooking;
