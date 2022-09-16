df47e72f7c529f920dfec9541a498184f3a97348
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-09-16 18:46:59 +0200

    #11 - refactor room types table

--- pages/admin/hotel-configuration/room-types.tsx
+++ pages/admin/hotel-configuration/room-types.tsx
@@ -27,6 +27,50 @@ const entries = [
   },
 ];
 
+const headers = [
+  {
+    id: 1,
+    name: 'Id',
+  },
+  {
+    id: 2,
+    name: 'Name',
+  },
+  {
+    id: 3,
+    name: 'Occupancy',
+  },
+  {
+    id: 4,
+    name: 'Price',
+  },
+  {
+    id: 5,
+    name: 'Action',
+  },
+];
+
+const roomTypes = [
+  {
+    id: 1,
+    name: 'Standard',
+    occupancy: 4,
+    price: '1200$',
+  },
+  {
+    id: 2,
+    name: 'Deluxe',
+    occupancy: 5,
+    price: '1500$',
+  },
+  {
+    id: 3,
+    name: 'Single',
+    occupancy: 1,
+    price: '1000$',
+  },
+];
+
 const RoomTypes = () => {
   const [selectedEntry, setSelectedEntry] = useState(entries[0]);
   return (
@@ -105,33 +149,35 @@ const RoomTypes = () => {
             <input className="border rounded py-1" />
           </div>
         </div>
-        <table className="table-auto w-full mt-5">
+        <table className="table-auto w-full mt-8">
           <thead className="text-left">
             <tr className="border-b">
-              <th>id</th>
-              <th>Name</th>
-              <th>Occupancy</th>
-              <th>Price</th>
-              <th>Action</th>
+              {headers.map((header) => (
+                <th key={header.id} className="pb-2">
+                  {header.name}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b">
-              <td>1</td>
-              <td>Standard</td>
-              <td>4</td>
-              <td>1200$</td>
-              <td className="w-40 py-3">
-                <div>
-                  <button className="bg-[#16D00B] text-white px-4 py-1 rounded-lg">
-                    Edit
-                  </button>
-                  <button className="bg-[#FC3532] text-white px-4 py-1 rounded-lg ml-4">
-                    Delete
-                  </button>
-                </div>
-              </td>
-            </tr>
+            {roomTypes.map((roomType) => (
+              <tr key={roomType.id} className="border-b">
+                <td>{roomType.id}</td>
+                <td>{roomType.name}</td>
+                <td>{roomType.occupancy}</td>
+                <td>{roomType.price}</td>
+                <td className="w-40 py-3">
+                  <div>
+                    <button className="bg-[#16D00B] text-white px-4 py-1 rounded-lg">
+                      Edit
+                    </button>
+                    <button className="bg-[#FC3532] text-white px-4 py-1 rounded-lg ml-4">
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
