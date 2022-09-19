f06e865167c2ed7b2db02cb6260c375c4ff5dffd
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-09-19 15:05:49 +0200

    #11 - Improve editable list

--- components/Admin/EditableList.tsx
+++ components/Admin/EditableList.tsx
@@ -21,9 +21,22 @@ const EditableList = () => {
 
   const addNewItem = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter' && e.target.value !== '') {
+      e.preventDefault();
       setItems([...items, { id: Date.now(), name: e.target.value }]);
       e.target.value = '';
     }
+  };
+
+  const updateItem = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === id) {
+        return { ...item, name: e.target.value };
+      }
+
+      return item;
+    });
+
+    setItems(updatedItems);
   };
 
   const removeItem = (id: number) => {
@@ -39,6 +52,7 @@ const EditableList = () => {
             type="text"
             className="border border-px rounded-lg py-2 px-3 w-4/5 focus:outline-none mt-3"
             value={item.name}
+            onChange={(e) => updateItem(e, item.id)}
             placeholder="Enter a value"
           />
           <button type="button" onClick={() => removeItem(item.id)}>
