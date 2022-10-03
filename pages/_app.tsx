b1918dcb6999e6836675a655bc1d550952af41a1
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-10-03 19:47:53 +0200

    #34 - react-query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from '../components/Layout';
 import AdminLayout from '../components/AdminLayout';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
+  const queryClient = new QueryClient();
 
   if (router.pathname.includes('admin')) {
     return (
-      <AdminLayout>
-        <Component {...pageProps} />
-      </AdminLayout>
+      <QueryClientProvider client={queryClient}>
+        <AdminLayout>
+          <Component {...pageProps} />
+        </AdminLayout>
+      </QueryClientProvider>
     );
   }
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 };
 
