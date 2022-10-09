796dc677dc1b4244a2fa4d93581b8879d350a8f8
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-10-09 21:33:25 +0200

    #36 - add room type deletion

--- pages/admin/hotel-configuration/room-types/index.tsx
+++ pages/admin/hotel-configuration/room-types/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import axios from 'axios';
+import { useMutation } from 'react-query';
 import Seo from '../../../../components/Seo';
 import Header from '../../../../components/Admin/Header';
 import AddButton from '../../../../components/Admin/AddButton';
@@ -43,59 +45,74 @@ interface RoomTypesProps {
   }[];
 }
 
-const RoomTypes: React.FC<RoomTypesProps> = ({ roomTypes }) => (
-  <div>
-    <Seo title="Room Types" />
-    <div className="flex justify-between">
-      <Header title="Room types" />
-      <AddButton name="room type" />
-    </div>
-    <div className="bg-white px-5 py-7 mt-8 rounded-lg">
-      <div className="flex justify-between flex-wrap gap-5">
-        <Entries />
-        <div className="flex items-center gap-3">
-          <p>Search</p>
-          <input className="border rounded py-1" />
+const RoomTypes: React.FC<RoomTypesProps> = ({ roomTypes }) => {
+  const { mutate } = useMutation((id: number) =>
+    axios.delete(`/room-types/${id}`)
+  );
+  const router = useRouter();
+
+  const deleteRoomType = (id: number) => {
+    mutate(id);
+    router.reload();
+  };
+
+  return (
+    <div>
+      <Seo title="Room Types" />
+      <div className="flex justify-between">
+        <Header title="Room types" />
+        <AddButton name="room type" />
+      </div>
+      <div className="bg-white px-5 py-7 mt-8 rounded-lg">
+        <div className="flex justify-between flex-wrap gap-5">
+          <Entries />
+          <div className="flex items-center gap-3">
+            <p>Search</p>
+            <input className="border rounded py-1" />
+          </div>
+        </div>
+        <div className="overflow-auto">
+          <table className="table-auto min-w-[500px] w-full mt-8">
+            <thead className="text-left">
+              <tr className="border-b">
+                {headers.map((header) => (
+                  <th key={header.id} className="pb-2">
+                    {header.name}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {roomTypes.map((roomType) => (
+                <tr key={roomType.id} className="border-b">
+                  <td>{roomType.id}</td>
+                  <td>{roomType.name}</td>
+                  <td>{roomType.occupancy}</td>
+                  <td>{roomType.price}</td>
+                  <td className="w-40 py-3">
+                    <div>
+                      <button className="bg-[#16D00B] text-white px-4 py-1 rounded-lg">
+                        Edit
+                      </button>
+                      <button
+                        className="bg-[#FC3532] text-white px-4 py-1 rounded-lg ml-4"
+                        onClick={() => deleteRoomType(roomType.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {roomTypes.length === 0 && (
+            <p className="text-center mt-5">No data available in table</p>
+          )}
         </div>
       </div>
-      <div className="overflow-auto">
-        <table className="table-auto min-w-[500px] w-full mt-8">
-          <thead className="text-left">
-            <tr className="border-b">
-              {headers.map((header) => (
-                <th key={header.id} className="pb-2">
-                  {header.name}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {roomTypes.map((roomType) => (
-              <tr key={roomType.id} className="border-b">
-                <td>{roomType.id}</td>
-                <td>{roomType.name}</td>
-                <td>{roomType.occupancy}</td>
-                <td>{roomType.price}</td>
-                <td className="w-40 py-3">
-                  <div>
-                    <button className="bg-[#16D00B] text-white px-4 py-1 rounded-lg">
-                      Edit
-                    </button>
-                    <button className="bg-[#FC3532] text-white px-4 py-1 rounded-lg ml-4">
-                      Delete
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {roomTypes.length === 0 && (
-          <p className="text-center mt-5">No data available in table</p>
-        )}
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RoomTypes;
