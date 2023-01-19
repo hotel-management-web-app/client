f5f5e1045e1547d93c52fee50d0eb0472f5b5ee2
JakubZakowicz <j.zakowicz99@gmail.com>
2023-01-19 11:41:58 +0100

    #82 - added room images display

--- components/RoomImages.tsx
+++ components/RoomImages.tsx
@@ -2,34 +2,11 @@ import React, { useState, useCallback } from 'react';
 import Image from 'next/image';
 import ImageViewer from 'react-simple-image-viewer';
 
-const images = [
-  {
-    id: 1,
-    url: '/../public/images/rooms.png',
-  },
-  {
-    id: 1,
-    url: '/../public/images/home-room-1.png',
-  },
-  {
-    id: 1,
-    url: '/../public/images/home-room-2.png',
-  },
-  {
-    id: 1,
-    url: '/../public/images/home-room-3.png',
-  },
-  {
-    id: 1,
-    url: '/../public/images/room-details.png',
-  },
-  {
-    id: 1,
-    url: '/../public/images/room.png',
-  },
-];
+interface RoomImagesProps {
+  images: string[];
+}
 
-const RoomImages = () => {
+const RoomImages: React.FC<RoomImagesProps> = ({ images }) => {
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
 
@@ -46,8 +23,9 @@ const RoomImages = () => {
     <div className="grid md:grid-cols-2 2xl:grid-cols-3 gap-20 mt-32">
       {images.map((image, index) => (
         <Image
-          key={image.id}
-          src={image.url}
+          key={image}
+          src={image}
+          loader={() => image}
           alt="room"
           width="450px"
           height="420px"
@@ -57,14 +35,7 @@ const RoomImages = () => {
       ))}
       {isViewerOpen && (
         <ImageViewer
-          src={[
-            'http://localhost:3000/images/rooms.png',
-            'http://localhost:3000/images/home-room-1.png',
-            'http://localhost:3000/images/home-room-2.png',
-            'http://localhost:3000/images/home-room-3.png',
-            'http://localhost:3000/images/room-details.png',
-            'http://localhost:3000/images/room.png',
-          ]}
+          src={images}
           currentIndex={currentImage}
           disableScroll={false}
           closeOnClickOutside
