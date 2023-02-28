649e85c299747de6f1eb19823e1d6758bf8d8fc6
JakubZakowicz <j.zakowicz99@gmail.com>
2023-02-28 22:09:52 +0100

    #122 - fix responsiveness in dashboard

--- components/Admin/Dashboard/RevenueChart.tsx
+++ components/Admin/Dashboard/RevenueChart.tsx
@@ -22,34 +22,37 @@ const RevenueChart: React.FC<RevenueChartProps> = ({ revenueData }) => {
   }));
 
   return (
-    <div className="w-[705px] bg-white rounded-xl shadow-lg pt-3 pb-10 h-[350px]">
+    <div className=" bg-white rounded-xl shadow-lg pt-3 pb-10 w-full max-w-[760px]">
       <h2 className="font-medium text-lg ml-5 mb-2">Revenues</h2>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={chartData}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="value"
-            name="Revenue [$]"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <div className="overflow-auto">
+        <div className="w-[760px] h-[300px]">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={chartData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="value"
+                name="Revenue [$]"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 };
