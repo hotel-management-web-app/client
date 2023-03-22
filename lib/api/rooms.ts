317ed1aa407dd7aac2d52d785e1f2b281d0b887d
JakubZakowicz <j.zakowicz99@gmail.com>
2023-03-22 11:29:45 +0100

    #144 - add rooms filtering

--- lib/api/rooms.ts
+++ lib/api/rooms.ts
@@ -3,8 +3,14 @@ import { Room, StatusesProps } from '../types';
 
 const baseUrl = '/rooms';
 
-export const getRooms = async (page?: number, limit?: number) =>
-  axios.get(`${baseUrl}?limit=${limit}&page=${page}`).then((res) => res.data);
+export const getRooms = async (
+  page?: number,
+  limit?: number,
+  search: string = ''
+) =>
+  axios
+    .get(`${baseUrl}?limit=${limit}&page=${page}&search=${search}`)
+    .then((res) => res.data);
 
 export const getRoom = async (id: number) =>
   axios.get(`${baseUrl}/${id}`).then((res) => res.data);
