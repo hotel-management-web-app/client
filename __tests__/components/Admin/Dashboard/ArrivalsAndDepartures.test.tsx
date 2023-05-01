e5678f43127d89decb93de75c32c8a90376e31cf
JakubZakowicz <j.zakowicz99@gmail.com>
2023-05-01 13:40:33 +0200

    #150 - add dashboard tests

---
+++ __tests__/components/Admin/Dashboard/ArrivalsAndDepartures.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ArrivalsAndDepartures from '../../../../components/Admin/Dashboard/ArrivalsAndDepartures';
+
+describe('ArrivalsAndDepartures', () => {
+  it('Should render properly', () => {
+    render(<ArrivalsAndDepartures data={arrivalsAndDeparturesToday} />);
+
+    const commentText = screen.getByTestId('arrivals-departures');
+
+    expect(commentText).toBeInTheDocument();
+  });
+});
+
+const arrivalsAndDeparturesToday = { arrivalsToday: [], departuresToday: [] };
