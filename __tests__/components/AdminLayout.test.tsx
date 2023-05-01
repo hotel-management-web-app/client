c0d957310a65fad284ff6571c27ab71225a1af05
JakubZakowicz <j.zakowicz99@gmail.com>
2023-05-01 22:23:35 +0200

    #150 - fix admin layout test

--- __tests__/components/AdminLayout.test.tsx
+++ __tests__/components/AdminLayout.test.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '@testing-library/jest-dom';
 import AdminLayout from '../../components/AdminLayout';
+
+const queryClient = new QueryClient();
 
 describe('AdminLayout', () => {
   it('Should render properly', () => {
     render(
-      <AdminLayout>
-        <div>
-          <p>Admin children</p>
-        </div>
-      </AdminLayout>
+      <QueryClientProvider client={queryClient}>
+        <AdminLayout>
+          <div>
+            <p>Admin children</p>
+          </div>
+        </AdminLayout>
+      </QueryClientProvider>
     );
 
     const pElement = screen.getByText(/Admin children/i);
