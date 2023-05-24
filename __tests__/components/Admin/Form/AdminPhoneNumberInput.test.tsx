ff43d067b3939d42f56921b487fc963c2936dca9
JakubZakowicz <j.zakowicz99@gmail.com>
2023-05-24 17:37:21 +0200

    #174 - add unit tests to phone number inputs

---
+++ __tests__/components/Admin/Form/AdminPhoneNumberInput.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MockWrapper from '../../../../__mocks__/MockWrapper';
+import PhoneNumberInput from '../../../../components/Admin/Form/PhoneNumberInput';
+
+describe('PhoneNumberInput', () => {
+  it('Should render properly', () => {
+    render(
+      <MockWrapper>
+        <PhoneNumberInput />
+      </MockWrapper>
+    );
+
+    const phoneNumberInputElement = screen.getByPlaceholderText('Phone number');
+
+    expect(phoneNumberInputElement).toBeInTheDocument();
+  });
+});
